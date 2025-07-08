3ec2177f9318702693485869afc6670d4730a8d2
Jose Segura <josed.segurav@gmail.com>
2025-07-07 20:13:06 -0500

    added active storage for img upload,added 5 customers

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,5 +1,44 @@
 ActiveAdmin.register Customer do
+  permit_params :name, :email, :phone, :notes, :image
 
-  permit_params :name, :phone, :email, :notes
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :email
+      f.input :phone
+      f.input :notes
+      f.input :image, as: :file
+    end
+    f.actions
+  end
 
+  show do
+    attributes_table do
+      row :name
+      row :email
+      row :phone
+      row :notes
+      row :image do |customer|
+        if customer.image.attached?
+          image_tag url_for(customer.image), width: "200"
+        else
+          "No image uploaded"
+        end
+      end
+    end
+  end
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :email
+    column :phone
+    column :image do |customer|
+      if customer.image.attached?
+        image_tag url_for(customer.image), width: "50"
+      end
+    end
+    actions
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,12 @@
 class Customer < ApplicationRecord
+    validates :name, :phone, presence: true
+    has_one_attached :image
+
     def self.ransackable_attributes(auth_object = nil)
         %w[id name email phone notes created_at updated_at]
+    end
+
+    def self.ransackable_associations(auth_object = nil)
+        %w[image_attachment image_blob]
     end
 end
